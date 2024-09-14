149c61703b59d581420d1f7d3deac09b99848606
Serigne koso Dieng <seugnkosso>
2024-09-14 16:17:36 +0000

    installer tailwind sur laravel 11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
-    <h1>welcome page</h1>
+    <h1 class="text-red-300">welcome page</h1>
     <ul>
         <li><a href="{{route('home')}}">Home</a></li>
-        <li><a href="{{route('about', [id=>12])}}">about</a></li>
+        <li><a href="{{route('about', 12)}}">about</a></li>
         <li><a href="{{route('products')}}">products</a></li>
     </ul>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolor vero at ullam fugiat voluptates quis, dolorem minima rerum? Qui dolores aliquid vel beatae quos sint in assumenda autem perspiciatis.</p>
