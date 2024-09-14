741083cbdd2f2e2469375648032c56be4a6b9ab0
Serigne koso Dieng <seugnkosso>
2024-09-14 10:30:42 +0000

    PREMIER MODIFICATION DE L'APPLICATION INIT

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, quidem maxime illum quibusdam ea reprehenderit at vel amet ab aliquid libero unde consequatur atque. Enim non fugit dicta dolore odit.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, quidem maxime illum quibusdam ea reprehenderit at vel amet ab aliquid libero unde consequatur atque. Enim non fugit dicta dolore odit.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, quidem maxime illum quibusdam ea reprehenderit at vel amet ab aliquid libero unde consequatur atque. Enim non fugit dicta dolore odit.</p>
+    <hr>
+
+    <footer>&copy; kosso beau 2024</footer>
+</body>
+</html>
