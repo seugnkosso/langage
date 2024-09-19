167633f9ab0d311b3d8c512a2e16295577385708
Serigne koso Dieng <seugnkosso>
2024-09-19 16:03:29 +0000

    les response dans laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,16 +8,33 @@ use Illuminate\Support\Facades\View;
 class ProductController extends Controller
 {
     public function index(){
+        // return "retourner un string"; //on peut retourner un string
         $nom = "kosso";
         $categorie = "dev web";
         $produits = ["ordinateur", "imprimante", "serveur"];
+        // return $nom; //on peut retourner une variable
+
+        // return redirect('products/create'); //on peut rediriger vers une autre page
+        // return redirect()->route('product.create'); //on peut rediriger vers une autre page avec le nom de la route
+        // return redirect()->away('https://www.youtube.com/watch?v=_UqOHEPkGms&list=RDEMrHRJ9PmBmTZM5Uxj6OFzMg&index=2'); //on peut rediriger vers ailleur
 
         return view('produits.index',
         [
             'name' => $nom,
             'categorie' => $categorie,
             'produits' => $produits
-        ]);
+        ]); //on peut retourner une view
+
+        // return response()->json(
+        //     [
+        //         'nom' => 'dieng',
+        //         'prenom' => 'Kosso',
+        //         'age' => '25'
+        //     ]
+        // ); //on peut retourner du json
+
+        // return response()->file(); on peut retourner du file
+
     }
 
     public function create(){
