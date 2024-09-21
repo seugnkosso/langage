365f2ba26097423ca5a31f7ea08cbad3a6878d90
Serigne koso Dieng <seugnkosso>
2024-09-21 17:54:47 +0000

    resource et collection

--- laravel_api/routes/web.php
+++ laravel_api/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\InvoiceController;
+use App\Http\Controllers\Api\V1\CustomerController;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix' =>'api/v1'], function () {
+    Route::apiResource('customers',CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
 });
