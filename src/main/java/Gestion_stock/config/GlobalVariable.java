67f5fb8e242dab90bd8e1e2f665655abdabd3583
Serigne koso Dieng <seugnkosso>
2024-09-25 12:09:40 +0000

    configuration des variable global

---
+++ src/main/java/Gestion_stock/config/GlobalVariable.java
@@ -0,0 +1,11 @@
+package Gestion_stock.config;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data // génère les getters et setters toString()...
+@RequiredArgsConstructor // génère constructeur all final attribut
+public class GlobalVariable {
+    public static final String urlFront = "http://localhost:4200";
+    public static final String size = "5";
+}
